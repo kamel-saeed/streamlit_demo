8ee19b11be280fc61feb889102ef95d8b92a26bc
kamel-saeed <107272450+kamel-saeed@users.noreply.github.com>
2022-09-24 20:44:10 +0200

    Add files via upload

---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('hello world')
+
+st.write('this is a test')
+
+st.markdown('''
+                - this is a test
+                - this is a test
+                - this is a test
+               
+''')
